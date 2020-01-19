d1f4defff9a7b225ced1d160844928c32855eebf
tflyons <thomaslyons77@gmail.com>
2020-01-18 20:39:33 -0800

    add example for partitioning

---
+++ examples/partitioning/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/haraqa/haraqa"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	topicPrefix := "mytopic"
+	numPartitions := 5
+	batchSize := 10
+
+	client, err := haraqa.NewClient(haraqa.DefaultConfig)
+	check(err)
+
+	ch := make(chan haraqa.ProduceMsg, batchSize)
+	for i := 0; i < numPartitions; i++ {
+		go func(topic []byte) {
+			ctx := context.Background()
+			err = client.ProduceLoop(ctx, topic, ch)
+			check(err)
+		}([]byte(fmt.Sprintf("%s-%d", topicPrefix, i)))
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go sendFiftyMessages(&wg, ch)
+	}
+	wg.Wait()
+}
+
+func sendFiftyMessages(wg *sync.WaitGroup, ch chan haraqa.ProduceMsg) {
+	for i := 0; i < 50; i++ {
+		msg := haraqa.NewProduceMsg([]byte(fmt.Sprintf("message number %d", i)))
+		ch <- msg
+		err := <-msg.Err
+		check(err)
+	}
+	wg.Done()
+}
