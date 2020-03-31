7305c42548431d197b22ac1bd62f3d5586dff247
tflyons <thomaslyons77@gmail.com>
2020-03-30 21:11:44 -0700

    cleanup data server tests

--- broker/data_server_test.go
+++ broker/data_server_test.go
@@ -12,158 +12,310 @@ import (
 )
 
 func TestDataServer(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 	var errMock = errors.New("mock error")
 	b := &Broker{}
 
-	conn := mocks.NewMockReadWriteCloser(gomock.NewController(t))
-	conn.EXPECT().Close().AnyTimes().Return(nil)
-	readCount := 0
-	conn.EXPECT().Read(gomock.Any()).AnyTimes().
-		DoAndReturn(func(b []byte) (int, error) {
-			defer func() { readCount++ }()
-			switch readCount {
-			case 0:
-				return 0, io.EOF
-			case 1:
-				return 0, errMock
-			case 2:
-				copy(b, []byte{0, protocol.TypeClose, 0, 0, 0, 0})
-				return 6, nil
-			case 3, 4:
-				copy(b, []byte{0, protocol.TypePing, 0, 0, 0, 0})
-				return 6, nil
-			case 5:
-				copy(b, []byte{0, protocol.TypeProduce, 0, 0, 0, 0})
-				return 6, nil
-			case 6:
-				copy(b, []byte{0, protocol.TypeConsume, 0, 0, 0, 0})
-				return 6, nil
-			}
-			panic("unexpected read")
-		})
-	writeCount := 0
-	conn.EXPECT().Write(gomock.Any()).AnyTimes().
-		DoAndReturn(func(b []byte) (int, error) {
-			defer func() { writeCount++ }()
-			switch writeCount {
-			case 0:
-				return 0, nil
-			case 1:
-				return 0, errMock
-			case 2:
-				return 6, nil
-			case 3, 4, 5:
-				return 0, nil
-			}
-			panic("unexpected write")
-		})
+	t.Run("read eof error", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).Return(0, io.EOF),
+			conn.EXPECT().Close().Return(nil),
+		)
+		b.handleDataConn(conn)
+	})
 
-		// read eof
-	b.handleDataConn(conn)
-	// read err
-	b.handleDataConn(conn)
-	// close prefix
-	b.handleDataConn(conn)
-	// ping prefix (error)
-	b.handleDataConn(conn)
-	// ping prefix (success) & produce prefix (error)
-	b.handleDataConn(conn)
-	// consume prefix (error)
-	b.handleDataConn(conn)
+	t.Run("read error", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).Return(0, errMock),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+			conn.EXPECT().Close().Return(nil),
+		)
+		b.handleDataConn(conn)
+	})
+
+	t.Run("read success, close prefix", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, []byte{0, protocol.TypeClose, 0, 0, 0, 0}), nil
+			}),
+			conn.EXPECT().Close().Return(nil),
+		)
+		b.handleDataConn(conn)
+	})
+
+	t.Run("read success, ping", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, []byte{0, protocol.TypePing, 0, 0, 0, 0}), nil
+			}),
+			conn.EXPECT().Write(gomock.Any()).Return(0, errMock),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+			conn.EXPECT().Close().Return(nil),
+		)
+		b.handleDataConn(conn)
+	})
+
+	t.Run("read success, produce", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, []byte{0, protocol.TypeProduce, 0, 0, 0, 0}), nil
+			}),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+			conn.EXPECT().Close().Return(nil),
+		)
+		b.handleDataConn(conn)
+	})
+	t.Run("read success, consume", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, []byte{0, protocol.TypeConsume, 0, 0, 0, 0}), nil
+			}),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+			conn.EXPECT().Close().Return(nil),
+		)
+		b.handleDataConn(conn)
+	})
 }
 
 func TestProduceHandler(t *testing.T) {
-	var errMock = errors.New("mock error")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	var (
+		errMock = errors.New("mock error")
+		b       = &Broker{
+			config: Config{MaxSize: 200},
+		}
+		produceReq = &protocol.ProduceRequest{}
+		buf        []byte
+		prefix     [6]byte
+	)
 
-	req1 := &protocol.ProduceRequest{
+	t.Run("read error", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).Return(0, errMock),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleProduce(conn, produceReq, &buf, 5, prefix[:])
+		if errors.Cause(err) != errMock {
+			t.Fatal(err)
+		}
+	})
+	t.Run("read success, invalid message", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, []byte{0, 0, 0, 0, 0}), nil
+			}),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleProduce(conn, produceReq, &buf, 5, prefix[:])
+		if errors.Cause(err).Error() != "invalid messages length" {
+			t.Fatal(err)
+		}
+	})
+
+	// serialize a valid message
+	buffer := new(bytes.Buffer)
+	err := (&protocol.ProduceRequest{
 		Topic:    []byte("produce-topic"),
 		MsgSizes: []int64{5, 6},
-	}
-	w1 := new(bytes.Buffer)
-	err := req1.Write(w1)
+	}).Write(buffer)
 	if err != nil {
 		t.Fatal(err)
 	}
-	msg1 := w1.Bytes()[6:]
+	msg := buffer.Bytes()[6:]
 
-	req2 := &protocol.ProduceRequest{
-		Topic:    []byte("produce-topic-2"),
-		MsgSizes: []int64{5, 6},
-	}
-	w2 := new(bytes.Buffer)
-	err = req2.Write(w2)
+	t.Run("read success, valid message, queue error", func(t *testing.T) {
+		mockQ := mocks.NewMockQueue(ctrl)
+		b.Q = mockQ
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			mockQ.EXPECT().Produce(gomock.Any(), gomock.Any(), gomock.Any()).Return(errMock),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleProduce(conn, produceReq, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err) != errMock {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("read success, valid message, queue success, write error", func(t *testing.T) {
+		mockQ := mocks.NewMockQueue(ctrl)
+		b.Q = mockQ
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			mockQ.EXPECT().Produce(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil),
+			conn.EXPECT().Write(gomock.Any()).Return(0, errMock).Times(2),
+		)
+		err := b.handleProduce(conn, produceReq, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err) != errMock {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("read success, valid message, queue success, write success", func(t *testing.T) {
+		mockQ := mocks.NewMockQueue(ctrl)
+		b.Q = mockQ
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			mockQ.EXPECT().Produce(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil),
+			conn.EXPECT().Write(gomock.Any()).Return(6, nil),
+		)
+		err := b.handleProduce(conn, produceReq, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err) != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("read success, message too largs", func(t *testing.T) {
+		b.config.MaxSize = 3
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleProduce(conn, produceReq, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err).Error() != "invalid message size. exceeds maximum limit of 3 bytes" {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestConsumeHandler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	var (
+		errMock     = errors.New("mock error")
+		b           = &Broker{}
+		consumeReq  = &protocol.ConsumeRequest{}
+		consumeResp = &protocol.ConsumeResponse{}
+		buf         []byte
+		prefix      [6]byte
+	)
+
+	t.Run("read error", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).Return(0, errMock),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleConsume(conn, consumeReq, consumeResp, &buf, 5, prefix[:])
+		if errors.Cause(err) != errMock {
+			t.Fatal(err)
+		}
+	})
+	t.Run("read success, invalid message", func(t *testing.T) {
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, []byte{0, 0, 0, 0, 0}), nil
+			}),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleConsume(conn, consumeReq, consumeResp, &buf, 5, prefix[:])
+		if errors.Cause(err).Error() != "invalid topic length" {
+			t.Fatal(err)
+		}
+	})
+
+	// serialize a valid message
+	buffer := new(bytes.Buffer)
+	err := (&protocol.ConsumeRequest{
+		Topic:  []byte("produce-topic"),
+		Offset: 0,
+		Limit:  2,
+	}).Write(buffer)
 	if err != nil {
 		t.Fatal(err)
 	}
-	msg2 := w2.Bytes()[6:]
+	msg := buffer.Bytes()[6:]
 
-	mockQ := mocks.NewMockQueue(gomock.NewController(t))
-	mockQ.EXPECT().Produce(gomock.Any(), req1.Topic, gomock.Any()).Return(errMock)
-	mockQ.EXPECT().Produce(gomock.Any(), req2.Topic, gomock.Any()).Times(2).Return(nil)
-	b := &Broker{
-		Q:      mockQ,
-		config: Config{MaxSize: 200},
-	}
-	produceReq := &protocol.ProduceRequest{}
-	var buf []byte
-	var prefix [6]byte
+	t.Run("read success, valid message, queue error", func(t *testing.T) {
+		mockQ := mocks.NewMockQueue(ctrl)
+		b.Q = mockQ
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			mockQ.EXPECT().ConsumeInfo(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, int64(0), nil, errMock),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleConsume(conn, consumeReq, consumeResp, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err) != errMock {
+			t.Fatal(err)
+		}
+	})
+	t.Run("read success, valid message, queue success, write 1 failure", func(t *testing.T) {
+		mockQ := mocks.NewMockQueue(ctrl)
+		b.Q = mockQ
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			mockQ.EXPECT().ConsumeInfo(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, int64(0), nil, nil),
+			conn.EXPECT().Write(gomock.Any()).Return(0, errMock),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+		)
+		err := b.handleConsume(conn, consumeReq, consumeResp, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err) != errMock {
+			t.Fatal(err)
+		}
+	})
 
-	conn := mocks.NewMockReadWriteCloser(gomock.NewController(t))
-	readCount := 0
-	conn.EXPECT().Read(gomock.Any()).AnyTimes().
-		DoAndReturn(func(b []byte) (int, error) {
-			defer func() { readCount++ }()
-			switch readCount {
-			case 0:
-				return 0, errMock
-			case 1:
-				copy(b, []byte{0, 0, 0, 0, 0})
-				return 5, nil
-			case 2:
-				n := copy(b, msg1)
-				return n, nil
-			case 3, 4:
-				n := copy(b, msg2)
-				return n, nil
-				//	case 3:
-				//		copy(b, w2.Bytes())
-				//		return w2.Len(), nil
-			}
-			panic("unexpected read")
-		})
-	writeCount := 0
-	conn.EXPECT().Write(gomock.Any()).AnyTimes().
-		DoAndReturn(func(b []byte) (int, error) {
-			defer func() { writeCount++ }()
-			switch writeCount {
-			case 0, 1, 2:
-				return 0, nil
-			case 3:
-				return 0, errMock
-			case 4, 5:
-				return 6, nil
-			}
-			panic("unexpected write")
-		})
+	t.Run("read success, valid message, queue success, write 1 success, queue failure", func(t *testing.T) {
+		mockQ := mocks.NewMockQueue(ctrl)
+		b.Q = mockQ
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			mockQ.EXPECT().ConsumeInfo(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, int64(0), []int64{0}, nil),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+			mockQ.EXPECT().Consume(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(errMock),
+		)
+		err := b.handleConsume(conn, consumeReq, consumeResp, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err) != errMock {
+			t.Fatal(err)
+		}
+	})
 
-	err = b.handleProduce(conn, produceReq, &buf, 5, prefix[:])
-	if errors.Cause(err) != errMock {
-		t.Fatal(err)
-	}
-	err = b.handleProduce(conn, produceReq, &buf, 5, prefix[:])
-	if errors.Cause(err).Error() != "invalid messages length" {
-		t.Fatal(err)
-	}
-	err = b.handleProduce(conn, produceReq, &buf, uint32(len(msg1)), prefix[:])
-	if errors.Cause(err) != errMock {
-		t.Fatal(err)
-	}
-	err = b.handleProduce(conn, produceReq, &buf, uint32(len(msg2)), prefix[:])
-	if errors.Cause(err) != errMock {
-		t.Fatal(err)
-	}
-	err = b.handleProduce(conn, produceReq, &buf, uint32(len(msg2)), prefix[:])
-	if errors.Cause(err) != nil {
-		t.Fatal(err)
-	}
+	t.Run("read success, valid message, queue success, write 1 success, queue success", func(t *testing.T) {
+		mockQ := mocks.NewMockQueue(ctrl)
+		b.Q = mockQ
+		conn := mocks.NewMockReadWriteCloser(ctrl)
+		gomock.InOrder(
+			conn.EXPECT().Read(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+				return copy(b, msg), nil
+			}),
+			mockQ.EXPECT().ConsumeInfo(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, int64(0), []int64{0}, nil),
+			conn.EXPECT().Write(gomock.Any()).Return(0, nil),
+			mockQ.EXPECT().Consume(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil),
+		)
+		err := b.handleConsume(conn, consumeReq, consumeResp, &buf, uint32(len(msg)), prefix[:])
+		if errors.Cause(err) != nil {
+			t.Fatal(err)
+		}
+	})
 }
