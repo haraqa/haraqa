7a7e346610fd5095bc57e78a5dbf2627b3d0fb3b
tflyons <thomaslyons77@gmail.com>
2020-07-30 23:15:04 -0700

    produce with goroutines

--- benchmarks/produce_test.go
+++ benchmarks/produce_test.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http/httptest"
 	"os"
+	"sync"
 	"testing"
 
 	"github.com/haraqa/haraqa/client"
@@ -52,6 +53,11 @@ func BenchmarkProduce(b *testing.B) {
 	b.Run("produce 10", benchProducer(10, c))
 	b.Run("produce 100", benchProducer(100, c))
 	b.Run("produce 1000", benchProducer(1000, c))
+	fmt.Println("")
+	b.Run("go produce 1", benchProducerN(10, 1, c))
+	b.Run("go produce 10", benchProducerN(10, 10, c))
+	b.Run("go produce 100", benchProducerN(10, 100, c))
+	b.Run("go produce 1000", benchProducerN(10, 1000, c))
 }
 
 func benchProducer(batchSize int, c *client.Client) func(b *testing.B) {
@@ -73,6 +79,42 @@ func benchProducer(batchSize int, c *client.Client) func(b *testing.B) {
 				b.Fatal(err)
 			}
 		}
+		b.StopTimer()
+	}
+}
+
+func benchProducerN(N, batchSize int, c *client.Client) func(b *testing.B) {
+	var err error
+	msgs := make([][100]byte, batchSize)
+	sizes := make([]int64, len(msgs))
+	var data []byte
+	for i := range msgs {
+		copy(msgs[i][:], []byte("something"))
+		data = append(data, msgs[i][:]...)
+		sizes[i] = int64(len(msgs[i]))
+	}
+	return func(b *testing.B) {
+		ch := make(chan struct{})
+		defer close(ch)
+		var wg sync.WaitGroup
+		for i := 0; i < N; i++ {
+			go func() {
+				for range ch {
+					body := bytes.NewBuffer(data)
+					err = c.Produce("benchtopic", sizes, body)
+					if err != nil {
+						b.Fatal(err)
+					}
+					wg.Done()
+				}
+			}()
+		}
+		b.ReportAllocs()
+		for i := 0; i < b.N; i += len(msgs) {
+			wg.Add(1)
+			ch <- struct{}{}
+		}
+		wg.Wait()
 		b.StopTimer()
 	}
 }
