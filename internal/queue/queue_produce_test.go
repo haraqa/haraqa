64ccb64c5313bd010f37765b98998e82a1dcc529
tflyons <thomaslyons77@gmail.com>
2021-01-09 15:38:06 -0800

    add queue producer tests

---
+++ internal/queue/queue_produce_test.go
@@ -0,0 +1,68 @@
+package queue
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/haraqa/haraqa/internal/headers"
+)
+
+func TestQueue_Produce(t *testing.T) {
+	t.Run("no cache", testQueueProduce(false))
+	t.Run("cached", testQueueProduce(true))
+}
+
+func testQueueProduce(cache bool) func(*testing.T) {
+	return func(t *testing.T) {
+		dirName, err := ioutil.TempDir("", ".haraqa*")
+		if err != nil {
+			t.Error(err)
+		}
+		defer func() {
+			if err := os.RemoveAll(dirName); err != nil {
+				t.Error(err)
+			}
+		}()
+		q, err := NewQueue([]string{dirName}, cache, 2)
+		if err != nil {
+			t.Error(err)
+		}
+		const topic = "topic"
+		if err = q.CreateTopic(topic); err != nil {
+			t.Error(err)
+		}
+
+		if err = q.Produce(topic, nil, 0, nil); err != nil {
+			t.Error(err)
+		}
+
+		msgs := []string{"my", "test", "messages", "are", "here"}
+		if err = produceMsgs(q, topic, msgs); err != nil {
+			t.Error(err)
+		}
+		if err = produceMsgs(q, topic, msgs); err != nil {
+			t.Error(err)
+		}
+
+		info, err := q.ModifyTopic(topic, headers.ModifyRequest{})
+		if err != nil {
+			t.Error(err)
+		}
+		if info.MinOffset != 0 || info.MaxOffset != int64(len(msgs)*2) {
+			t.Error(info)
+		}
+	}
+}
+
+func produceMsgs(q *Queue, topic string, msgs []string) error {
+	r := new(bytes.Buffer)
+	var msgSizes []int64
+	for _, msg := range msgs {
+		msgSizes = append(msgSizes, int64(len(msg)))
+		r.WriteString(msg)
+	}
+	return q.Produce(topic, msgSizes, uint64(time.Now().Unix()), r)
+}
